36d3e2dca718c4e4cb8cbef81b4cc9acdf71e1ac
Raj Namdev <128510047+Rajnamdev@users.noreply.github.com>
2024-07-26 22:06:09 +0530

    Add files via upload
    final

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+// Importing necessary React modules and components
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { fetchDataFromApi } from './utils/api';
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration, getGenres } from './store/homeSlice';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult';
+import Explore from './pages/explore/Explore';
+import PageNotFound from './pages/404/PageNotFound';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './Signup';
+import Login from './Login';
+
+
+
+// Function component for the main App
+function App() {
+  // Initializing Redux dispatch and selecting URL from Redux state
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+
+  // useEffect to fetch API configuration and genres on component mount
+  useEffect(() => {
+    fetchApiConfig();
+    genresCall();
+  }, []);
+
+  // Function to fetch API configuration
+  const fetchApiConfig = () => {
+    fetchDataFromApi('/configuration').then((res) => {
+      console.log(res);
+
+      // Creating URLs for different image types
+      const apiUrl = {
+        backdrop: res.images.secure_base_url + 'original',
+        poster: res.images.secure_base_url + 'original',
+        profile: res.images.secure_base_url + 'original',
+      };
+
+      // Dispatching the obtained configuration to Redux store
+      dispatch(getApiConfiguration(apiUrl));
+    });
+  };
+
+  // Function to fetch genres for both TV and movies
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ['tv', 'movie'];
+    let allGenres = {};
+
+    // Creating promises for fetching genres
+    endPoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    // Resolving promises and updating the Redux store with genres
+    const data = await Promise.all(promises);
+    data.map(({ genres }) => {
+      return genres.map((item) => {
+        allGenres[item.id] = item;
+      });
+    });
+
+    dispatch(getGenres(allGenres));
+  };
+
+  // Rendering the main application with React Router
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path='/register' element={<Signup />} />
+        <Route path='/' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          THIS IS SITE HAS BEEN DESIGN AND DEVELOPED BY RAJ,HRISHABH AND
+          PRASHANT
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon" a>
+            <FaInstagram
+              onClick={() =>
+                window.open(
+                  "https://www.instagram.com/prashant_the_one/",
+                  "_blank"
+                )
+              }
+            />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
